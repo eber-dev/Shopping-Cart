2083ec1f2456e3e2b3c3c599e86ed70e0dfcf3ec
Eber <www.eberacunaquispe@gmail.com>
2026-07-27 18:47:43 -0500

    Se elimino App ya que router se encargara del renderizado completo y se importo las clases router provider para poder renderizar todo

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import { RouterProvider } from 'react-router-dom';
+import Router from './routes/AppRoutes.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={Router} />
     </StrictMode>,
 );
