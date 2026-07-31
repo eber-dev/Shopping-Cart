612dddee0395e552d389d345a9eaffff4ac456e9
Eber <www.eberacunaquispe@gmail.com>
2026-07-30 22:45:21 -0500

    Se asigno el archivo css de estilos para el layout principal

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,13 @@
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router-dom';
+import style from './Layout.module.css';
 
 function Layout() {
     return (
-        <>
+        <div className={style.principal}>
             <Navbar />
             <Outlet />
-        </>
+        </div>
     );
 }
 
