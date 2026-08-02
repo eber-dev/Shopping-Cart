11856f97f50fde5393f41df641ce8726ee9f3f3a
Eber <www.eberacunaquispe@gmail.com>
2026-08-02 16:58:39 -0500

    Se cambio el tamaño del navbar y tambien se le añadio un icono principal asimismo se regulo la altura que ocuparas el resto de componentes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,25 @@
 import { NavLink } from 'react-router-dom';
 import style from './Navbar.module.css';
+import icono from '../assets/icono1.png';
 
 function Navbar() {
     return (
         <nav className={style.navegacion}>
-            <NavLink className={style.opcion} to="/">
-                Home
-            </NavLink>
-            <NavLink className={style.opcion} to="/shop">
-                Shop
-            </NavLink>
-            <NavLink className={style.opcion} to="/cart">
-                Cart
-            </NavLink>
+            <div className={style.logo}>
+                <img src={icono} alt="logo principal" />
+            </div>
+
+            <div className={style.opciones}>
+                <NavLink className={style.opcion} to="/">
+                    Home
+                </NavLink>
+                <NavLink className={style.opcion} to="/shop">
+                    Shop
+                </NavLink>
+                <NavLink className={style.opcion} to="/cart">
+                    Cart
+                </NavLink>
+            </div>
         </nav>
     );
 }
