3ec6769cb3ec63bfa2c5f0d22b6f4e9c54de089c
Eber <www.eberacunaquispe@gmail.com>
2026-08-02 16:18:55 -0500

    Asiganacion de clases a las etiqeutas html del componente

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import style from './Home.module.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
         <div className={style.caja}>
-            <h1>hola</h1>
+            <div>
+                <h1 className={style.titulo}>
+                    Todo lo que necesitas en tecnología
+                </h1>
+                <p className={style.parrafo}>
+                    Conoce nuestra colección de dispositivos tecnológicos y
+                    encuentra el equipo ideal para ti.
+                </p>
+                <Link to="/shop">
+                    <button className={style.catalogo}>Ver Productos</button>
+                </Link>
+            </div>
         </div>
     );
 }
